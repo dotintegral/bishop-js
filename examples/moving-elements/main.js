deb8154b8d69c75b0e4a82cb4836c88430b438d9
Artur Siery <dotintegral@gmail.com>
2015-11-09 19:55:28 +0100

    Added example with moving elements

---
+++ examples/moving-elements/main.js
@@ -0,0 +1,20 @@
+define([
+    '../../bishop'
+], function (
+    bishop
+) {
+    "use strict";
+
+    // To play around in console
+    window.bishop = bishop;
+
+    document.addEventListener('keydown', bishop.keyHandler);
+
+    // override initial settings:
+
+    bishop.step = 30;
+    bishop.dispersion = 50;
+    bishop.debugPoints = false;
+
+    bishop.init();
+});
